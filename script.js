990a9d9cc9152cbf883e5c3e6f1a1a3f0611eb37
gaven <gaven.desrosiers@gmail.com>
2026-09-19 22:01:50 -0500

    added icon

--- script.js
+++ script.js
@@ -20,17 +20,29 @@ function checkTime() {
 	let seconds = Math.ceil((remainingMilliseconds / 1000) % 60);
 	console.log(minutes);
 	console.log(seconds);
+	console.log(remainingMilliseconds)
 	if (remainingMilliseconds >= 0) {
 		setTimeout(checkTime, 1000);
 		timerDisplay.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
 	}
 	if (remainingMilliseconds <= 174000 && outroNotPlayed) {
 		outroChanting.play();
+		console.log("Playing Closing Audio");
 		outroNotPlayed = false;
 	}
 }
+
 startBtn.addEventListener("click", function() {
 	checkTime();
+
+	outroChanting.play().then(() => {
+		outroChanting.pause();
+		outroChanting.currentTime = 0;
+	}).catch(error => {
+		console.log("Audio unlock failed:", error);
+	});
+
 	introChanting.play();
+	console.log("Playing Closing Audio");
 	requestWakeLock();
 });
