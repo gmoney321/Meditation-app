cc8949ad8b6671a101804503174d1045b0b917ec
gaven <gaven.desrosiers@gmail.com>
2026-09-19 20:16:46 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let wakeLock = null;
+let outroNotPlayed = true;
+const targetTime = Date.now() + 1800000;
+const timerDisplay = document.querySelector(".timer");
+const startBtn = document.querySelector(".start-btn");
+const outroChanting = new Audio('src_assets_audio_closing-chanting.mp3');
+const introChanting = new Audio('src_assets_audio_intro-chanting.mp3');
+async function requestWakeLock() {
+	try {
+		wakeLock = await navigator.wakeLock.request('screen');
+		console.log('Screen Wake Lock is active!');
+	} catch (err) {
+		console.error(`Wake Lock error: ${err.name} ${err.message}`);
+	}
+}
+
+function checkTime() {
+	const remainingMilliseconds = targetTime - Date.now();
+	let minutes = Math.floor(remainingMilliseconds / 60000);
+	let seconds = Math.ceil((remainingMilliseconds / 1000) % 60);
+	console.log(minutes);
+	console.log(seconds);
+	if (remainingMilliseconds >= 0) {
+		setTimeout(checkTime, 1000);
+		timerDisplay.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+	}
+	if (remainingMilliseconds <= 174000 && outroNotPlayed) {
+		outroChanting.play();
+		outroNotPlayed = false;
+	}
+}
+startBtn.addEventListener("click", function() {
+	checkTime();
+	introChanting.play();
+	requestWakeLock();
+});
